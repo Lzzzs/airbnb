61bd8907053bcb4d10f9219df9af2ee46da6c80e
liangzhensheng <1250564179@qq.com>
2022-12-23 17:02:00 +0800

    feat: :sparkles: 引入antd,完善了room-item

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -3,7 +3,21 @@ import { RoomItemWrapper } from './style';
 
 const RoomItem = memo((props) => {
   const { itemData } = props;
-  return <RoomItemWrapper>{itemData.name}</RoomItemWrapper>;
+
+  return (
+    <RoomItemWrapper
+      verifyColor={itemData?.verify_info?.text_color || '#39576a'}
+    >
+      <div className="inner">
+        <div className="cover">
+          <img src={itemData.picture_url} alt="" />
+        </div>
+        <div className="desc">{itemData.verify_info.messages.join(' · ')}</div>
+        <div className="name">{itemData.name}</div>
+        <div className="price">¥{itemData.price}/晚</div>
+      </div>
+    </RoomItemWrapper>
+  );
 });
 
 export default RoomItem;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import 'antd/dist/reset.css';
 
 import App from './App';
 import 'normalize.css';
