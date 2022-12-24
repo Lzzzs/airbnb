fd0f83ee94bac0d5395c3237814849806e4b745c
liangzhensheng <1250564179@qq.com>
2022-12-24 20:23:00 +0800

    style: :lipstick: 修改tabs的样式

--- src/components/section-tabs/style.js
+++ src/components/section-tabs/style.js
@@ -3,20 +3,23 @@ import styled from 'styled-components';
 export const SectionTabsWrapper = styled.div`
   display: flex;
   margin: 0px -8px;
+  overflow-x: auto;
+
   .tab {
-    flex: 1;
     border: solid 0.5px #d8d8d8;
-    box-sizing: border-box;
     padding: 14px 16px;
     border-radius: 5px;
     height: 48px;
     box-shadow: 0px 1px 2px rgb(0 0 0 / 15%);
     cursor: pointer;
     margin: 8px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     font-size: 17px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    flex-basis: 120px;
+    flex-shrink: 0;
 
     ${(props) => props.theme.mixin.boxShadow}
   }
